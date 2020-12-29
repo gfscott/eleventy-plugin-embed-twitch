054b706cf114e37cae7270543a685fb40fc085b3
Graham F. Scott <gfscott@gmail.com>
2020-12-29 16:07:34 -0500

    init rome, fix style

--- test/inc/invalidStrings.js
+++ test/inc/invalidStrings.js
@@ -1,6 +1,15 @@
 module.exports = [
-  {type: 'User, With prepended text', str: 'foo https://www.twitch.tv/vixella'},
-  {type: 'User, With prepended text, with link', str: 'foo <a href="">https://www.twitch.tv/vixella</a>'},
-  {type: 'Archived, With prepended text', str: 'foo https://www.twitch.tv/videos/597008599'},
-  {type: 'Archived, With prepended text, with link', str: 'foo <a href="">https://www.twitch.tv/videos/597008599</a>'},
+	{type: "User, With prepended text", str: "foo https://www.twitch.tv/vixella"},
+	{
+		type: "User, With prepended text, with link",
+		str: 'foo <a href="">https://www.twitch.tv/vixella</a>',
+	},
+	{
+		type: "Archived, With prepended text",
+		str: "foo https://www.twitch.tv/videos/597008599",
+	},
+	{
+		type: "Archived, With prepended text, with link",
+		str: 'foo <a href="">https://www.twitch.tv/videos/597008599</a>',
+	},
 ];
--- test/inc/validStrings.js
+++ test/inc/validStrings.js
@@ -1,14 +1,20 @@
 module.exports = [
-  // user channels
-  {type: 'User', str: 'https://www.twitch.tv/vixella'},
-  {type: 'User, With http', str: 'http://www.twitch.tv/vixella'},
-  {type: 'User, Without protocol', str: 'www.twitch.tv/vixella'},
-  {type: 'User, With https, without www', str: 'https://twitch.tv/vixella'},
-  {type: 'User, With http, without www', str: 'http://twitch.tv/vixella'},
-  // archived videos
-  {type: 'Archived', str: 'https://www.twitch.tv/videos/597008599'},
-  {type: 'Archived, With http', str: 'http://www.twitch.tv/videos/597008599'},
-  {type: 'Archived, Without protocol', str: 'www.twitch.tv/videos/597008599'},
-  {type: 'Archived, With https, without www', str: 'https://twitch.tv/videos/597008599'},
-  {type: 'Archived, With http, without www', str: 'http://twitch.tv/videos/597008599'},
+	// user channels
+	{type: "User", str: "https://www.twitch.tv/vixella"},
+	{type: "User, With http", str: "http://www.twitch.tv/vixella"},
+	{type: "User, Without protocol", str: "www.twitch.tv/vixella"},
+	{type: "User, With https, without www", str: "https://twitch.tv/vixella"},
+	{type: "User, With http, without www", str: "http://twitch.tv/vixella"},
+	// archived videos
+	{type: "Archived", str: "https://www.twitch.tv/videos/597008599"},
+	{type: "Archived, With http", str: "http://www.twitch.tv/videos/597008599"},
+	{type: "Archived, Without protocol", str: "www.twitch.tv/videos/597008599"},
+	{
+		type: "Archived, With https, without www",
+		str: "https://twitch.tv/videos/597008599",
+	},
+	{
+		type: "Archived, With http, without www",
+		str: "http://twitch.tv/videos/597008599",
+	},
 ];
