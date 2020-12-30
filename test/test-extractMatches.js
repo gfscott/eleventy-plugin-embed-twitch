fb09b083703d7ae25c68216043b50f87ab802cbe
Graham F. Scott <gfscott@gmail.com>
2020-12-29 19:53:37 -0500

    finish extract test

--- test/test-extractMatches.js
+++ test/test-extractMatches.js
@@ -157,3 +157,171 @@ archive.forEach(function(obj) {
 		},
 	);
 });
+
+
+/**
+ * =============================================
+ * USER STANDARD TESTS: Inline options specified
+ * ---------------------------------------------
+ */
+let userInlineOptionsTrue = {
+	id: "vixella",
+	type: "channel",
+	options: {
+		aspectRatio: "4:3",
+		autoplay: true,
+		closedCaptions: true,
+	},
+};
+
+user.forEach(function(obj) {
+	/**
+   * TEST: Recognizes valid strings wrapped in paragraph tags
+   */
+	test(
+		`${obj.type} ideal case, with inline options`,
+		(t) => {
+			let idealCase = `<p>${obj.str} 4:3 cc autoplay</p>`;
+			t.deepEqual(
+				extractMatches(patternPresent(idealCase)),
+				userInlineOptionsTrue,
+			);
+		},
+	);
+
+	/**
+   * TEST: Recognizes valid strings wrapped in paragraph tags, with
+   * additional whitespace
+   */
+	test(
+		`${obj.type} with whitespace, with inline options`,
+		(t) => {
+			let withWhitespace = `<p>
+        ${obj.str} 4:3 cc 
+        autoplay
+      </p>`;
+			t.deepEqual(
+				extractMatches(patternPresent(withWhitespace)),
+				userInlineOptionsTrue,
+			);
+		},
+	);
+
+	/**
+   * TEST: Recognizes valid strings wrapped in paragraph and anchor tags
+   */
+	test(
+		`${obj.type} with links, with inline options`,
+		(t) => {
+			let withLinks = `<p><a href="">${obj.str}</a> 4:3 cc auto</p>`;
+			t.deepEqual(
+				extractMatches(patternPresent(withLinks)),
+				userInlineOptionsTrue,
+			);
+		},
+	);
+
+	/**
+   * TEST: Recognizes valid strings wrapped in paragraph and anchor tags,
+   * with additional whitespace
+   */
+	test(
+		`${obj.type} with links and whitespace, with inline options`,
+		(t) => {
+			let withLinksAndWhitespace = `<p>
+        <a href="">
+          ${obj.str}
+        </a> 4:3 auto
+        cc
+      </p>`;
+			t.deepEqual(
+				extractMatches(patternPresent(withLinksAndWhitespace)),
+				userInlineOptionsTrue,
+			);
+		},
+	);
+});
+
+/**
+ * ================================================
+ * ARCHIVE STANDARD TESTS: Inline options specified
+ * ------------------------------------------------
+ */
+
+let archiveInlineOptionsTrue = {
+	id: "597008599",
+	type: "video",
+	options: {
+		aspectRatio: "4:3",
+		autoplay: true,
+		closedCaptions: true,
+	},
+};
+
+archive.forEach(function(obj) {
+	/**
+   * TEST: Recognizes valid strings wrapped in paragraph tags
+   */
+	test(
+		`${obj.type} ideal case, with inline options`,
+		(t) => {
+			let idealCase = `<p>${obj.str} 4:3 autoplay cc</p>`;
+			t.deepEqual(
+				extractMatches(patternPresent(idealCase)),
+				archiveInlineOptionsTrue,
+			);
+		},
+	);
+
+	/**
+   * TEST: Recognizes valid strings wrapped in paragraph tags, with
+   * additional whitespace
+   */
+	test(
+		`${obj.type} with whitespace, with inline options`,
+		(t) => {
+			let withWhitespace = `<p>
+        ${obj.str} 4:3 auto
+        CC
+      </p>`;
+			t.deepEqual(
+				extractMatches(patternPresent(withWhitespace)),
+				archiveInlineOptionsTrue,
+			);
+		},
+	);
+
+	/**
+   * TEST: Recognizes valid strings wrapped in paragraph and anchor tags
+   */
+	test(
+		`${obj.type} with links, with inline options`,
+		(t) => {
+			let withLinks = `<p><a href="">${obj.str}</a> autoplay 4:3 cc</p>`;
+			t.deepEqual(
+				extractMatches(patternPresent(withLinks)),
+				archiveInlineOptionsTrue,
+			);
+		},
+	);
+
+	/**
+   * TEST: Recognizes valid strings wrapped in paragraph and anchor tags,
+   * with additional whitespace
+   */
+	test(
+		`${obj.type} with links and whitespace, with inline options`,
+		(t) => {
+			let withLinksAndWhitespace = `<p>
+        <a href="">
+          ${obj.str}
+        </a> auto
+        CC 4:3
+      </p>`;
+			t.deepEqual(
+				extractMatches(patternPresent(withLinksAndWhitespace)),
+				archiveInlineOptionsTrue,
+			);
+		},
+	);
+});
